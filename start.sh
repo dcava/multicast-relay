753c13b8d40aecf6af31cc8294d6b48c9ef0e618
David Cavallucci <dcava@users.noreply.github.com>
2022-01-09 20:34:18 +1000

    Remove --foreground option from start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
 echo "starting multicast-relay"
 echo "Using Interfaces:" $INTERFACES
-echo "Using Options --foreground " $OPTS
-python3 ./multicast-relay/multicast-relay.py --interfaces $INTERFACES --foreground $OPTS
+echo "Using Options:" $OPTS
+python3 ./multicast-relay/multicast-relay.py --interfaces $INTERFACES $OPTS
